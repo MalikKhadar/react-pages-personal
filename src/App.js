9098521af0120bcd352f9ae858fd94c0a5b1fc0b
MalikKhadar <khada005@umn.edu>
2024-05-01 13:42:06 -0500

    Minor <p> test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Also, if you see this. ur epic! XD
         </p>
         <a
           className="App-link"
